e34e5c67596eebedcf941c7ad3c2de6cde6fca3a
Trevor <trevcj@gmail.com>
2018-02-02 18:42:25 -0700

    updated menu still is in progress

--- assets/javascript/javascriptIndex.js
+++ assets/javascript/javascriptIndex.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-	
-
+	$("#menu").on("click", function(){
+		console.log("working!");
+		if ($(".slideOut").hasClass("active")) {
+			$(".slideOut").removeClass("active");
+			$(".slideOut").hide(400);
+		} else{
+			$(".slideOut").addClass("active");
+			$(".slideOut").show(400);
+		}
+	});
 
 
 });
