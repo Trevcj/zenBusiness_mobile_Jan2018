859c52f4a8370e05ef64f95897743f7ba1cfd7ce
Trevor <trevcj@gmail.com>
2018-02-07 12:22:55 -0700

    some more changes

--- assets/javascript/javascriptIndex.js
+++ assets/javascript/javascriptIndex.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
 			$(".slideOut").removeClass("active");
 			$("#links").fadeOut(100);
 			$("#slideSignIn").fadeOut(100);
+			$("#menu").removeClass("white");
 			
 		} else{
 			$(".slideOut").addClass("active");
 			$("#links").delay(100).fadeIn(300);
 			$("#slideSignIn").delay(100).fadeIn(300);
+			$("#menu").addClass("white");
 		}
 	});
 
